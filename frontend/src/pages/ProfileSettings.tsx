c98795253d168731502f035cf71c69c6fa1b5c61
sishi-at-tu-chemnitz.de <sindhura.shivaprasad@s2023.tu-chemnitz.de>
2024-12-25 19:31:34 +0100

    #27 Password and Email edit restriction for google based logins

--- frontend/src/pages/ProfileSettings.tsx
+++ frontend/src/pages/ProfileSettings.tsx
@@ -1,9 +1,10 @@
-// import { Sidebar } from "../components/sidebar"; // Adjust the import path if necessary
-import ProfileForm from "../components/profile/form"; // Adjust the import path if necessary
+import ProfileForm from "../components/profile/form";
 import { Separator } from "@components/ui/separator";
 import PassowrdForm from "../components/passsword/password-form";
+import useStore from "../store/store";
 
 const ProfileSettings = () => {
+	const user = useStore((state) => state.user);
 	return (
 		<div className="flex flex-1 flex-col lg:flex-row overflow-hidden bg-background">
 			<div className="flex flex-1 flex-col lg:flex-row">
@@ -11,11 +12,17 @@ const ProfileSettings = () => {
 				<main className="flex-1 p-6 overflow-y-auto">
 					<h1 className="text-2xl font-semibold mb-6">Accounts</h1>
 					<ProfileForm />
-					<div className="my-6">
-						<Separator />
-					</div>
-					<h1 className="text-2xl font-semibold mb-6">Passwords</h1>
-					<PassowrdForm />
+					{user?.authProvider !== "google" && (
+						<>
+							<div className="my-6">
+								<Separator />
+							</div>
+							<h1 className="text-2xl font-semibold mb-6">
+								Passwords
+							</h1>
+							<PassowrdForm />
+						</>
+					)}
 				</main>
 			</div>
 		</div>
