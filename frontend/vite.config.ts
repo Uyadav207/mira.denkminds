0f9460b896acda3c0003900c9c65672f1eaf2cde
uty-at-tu-chemnitz.de <utkarsh.yadav@s2023.tu-chemnitz.de>
2025-01-25 03:14:58 +0100

    #69 Re-coonfigure Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,33 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": "/src",
-			"@components": "/src/components",
-			"@api": "/src/api",
-			"@lib": "/src/lib",
-			"@store": "/src/store",
-			"@pages": "/src/pages",
-			"@types": "/src/types",
-			"@hooks": "/src/hooks",
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	const BASE_URL = env.VITE_BASE_URL || "/";
+
+	return {
+		base: BASE_URL,
+		plugins: [react()],
+		resolve: {
+			alias: {
+				"@": "/src",
+				"@components": "/src/components",
+				"@api": "/src/api",
+				"@lib": "/src/lib",
+				"@store": "/src/store",
+				"@pages": "/src/pages",
+				"@types": "/src/types",
+				"@hooks": "/src/hooks",
+			},
 		},
-	},
-	server: {
-		port: 3000,
-		fs: {
-			allow: [".."],
+		server: {
+			port: 3000,
+			fs: {
+				allow: [".."],
+			},
 		},
-	},
-	define: {
-		"process.env": process.env,
-	},
+		define: {
+			"process.env": env,
+		},
+	};
 });
